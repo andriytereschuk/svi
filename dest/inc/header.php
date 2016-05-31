3bc7b570b0b2606016f4a1cde8cb129516fb84e4
andriytereschuk <shatskyat@gmail.com>
2016-05-31 13:13:22 +0300

    setup gulp and postcss

---
+++ dest/inc/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <?php include_once('title/main.php'); ?>
+
+    <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+    <meta name="author" content="Андрій Терещук">
+    <meta name="robots" content="all">
+
+    <link rel="shortcut icon" href="images/favicon.ico"/>
+
+    <link rel="stylesheet" href="css/dev.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-rc1/jquery.min.js"></script>
+</head>
+<body>
+
+<header></header>
---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var cssnext = require('postcss-cssnext');
+var cssnano = require('cssnano');
+var short = require('postcss-short');
+
+var browserSync = require('browser-sync').create();
+
+var iconfontCss = require('gulp-iconfont-css');
+var iconfont = require('gulp-iconfont');
+
+gulp.task('css', function () {
+    var processors = [
+        cssnext,
+        short,
+        // cssnano(),
+    ];
+    return gulp.src('./build/cssnext/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dest/css/'));
+})
+
+
+// Static Server + watching scss/html files
+gulp.task('default', ['css'], function () {
+
+    browserSync.init({
+        proxy: 'http://localhost/svi'
+    });
+
+    gulp.watch('./build/cssnext/*.css', ['css']);
+    gulp.watch('./dest/**.php').on('change', browserSync.reload);
+});
+
+
+// svg to font and generate scss
+var fontName = 'icons';
+
+gulp.task('iconfont', function () {
+    gulp.src(['build/icons/*.svg'], { base: './' })
+        .pipe(iconfontCss({
+            fontName: fontName,
+            path: 'templates/_icons.css',
+            targetPath: '../scss/_icons.scss',
+            fontPath: '../fonts/',
+        }))
+        .pipe(iconfont({
+            fontName: fontName,
+            normalize: true
+        }))
+        .pipe(gulp.dest('fonts'));
+});
